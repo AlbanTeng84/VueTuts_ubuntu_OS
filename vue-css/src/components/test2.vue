4d468d8dcd438b53b936487895fea2a10905e8a9
albanTeng84 <albanteng84@gmail.com>
2023-02-26 15:23:34 +0800

    update-3:23

---
+++ vue-css/src/components/test2.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }} - {{ title.company }}</h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            msg:"This is test2 page"
+        }
+    },
+    props:['title']
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-css/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>This is home page</h1>
+  <test2 :title="title"/>
+</template>
+
+<script>
+import test2 from '../components/test2.vue'
+
+export default {
+  name: 'HomeView',
+  components: {test2},
+  data(){
+    return{
+      title:{
+        name:'Ping',
+        company:'Zhao'
+        }
+    }
+  }
+
+}
+
+</script>
