a4c00c1fe588d5b9ae60450eb8cdc7805de01d70
albanTeng84 <albanteng84@gmail.com>
2023-03-06 17:00:43 +0800

    updated

---
+++ test-vue-binding/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <img alt="Vue logo" src="https://picsum.photos/200/300">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <a :href="URL">Labrary</a>
+  <Emit @customer-change="handleCustomChange"/>
+  <p>Uppercase: {{ uppercase }}</p>
+</template>
+
+<script>
+
+import Emit from "./components/Emit.vue"
+export default {
+  name: 'App',
+  components: {Emit
+  },
+  data(){
+    return{
+      URL:"https://picsum.photos/200/300",
+      uppercase: ''
+
+    }
+  },
+  methods: {
+    handleCustomChange (s) {
+      this.uppercase = s
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-css/src/components/Button.vue
@@ -0,0 +1,90 @@
+<template>
+    <h1>{{msg}}</h1>
+    <div class="container">
+        <button class="btn btn-success">Click me</button>
+        <button>Click me</button>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-8">
+                <div class="card">
+                    <img src="https://fastly.picsum.photos/id/276/200/300.jpg?hmac=5VSJJxPMnu25ukKlrNc0y2KxLtG_jF7BfA13IlTm0uo" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+        
+    </div>
+
+    <div class="contiainer">
+        <div class="row" style="width:1000px">
+            <div class="col align-self-end">
+                <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="https://picsum.photos/id/237/200/300" class="d-block w-30" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="https://picsum.photos/seed/picsum/200/300" class="d-block w-20" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="https://picsum.photos/200/300?grayscale" class="d-block w-20" alt="...">
+                </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+            </div>
+        </div>
+    </div>
+
+   
+    <div class="container">
+        <div class="position-relat">
+            <div class="position-absolute top-0 end-0">
+                <table class="table">
+                <tr>
+                    <th scope="col" :href="Player">{{Player}}</th>
+                    <th scope="col">Googles</th>
+                    <th scope="col">ZA</th>
+                </tr>
+                </table>
+            </div>
+        </div>
+        
+
+    </div>
+
+    <div class="position-relative">
+            <div class="position-absolute bottom-0 end-0">
+                <a :href="Player"></a>
+            </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data(){
+        return{
+            msg:"this is testing Button",
+            Player:"https://picsum.photos/200/300?grayscale"
+            
+        }
+    }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
